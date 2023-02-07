4a4478159078fbf293dd0935859a961213f54d16
wsauvage <william.sauvage@cib-prod.com>
2023-02-07 11:43:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <ul>
+            <li><a href="exercices/exercice1.php">Exercice 1 : vous êtes conneté !</a></li>
+            <li><a href="exercices/exercice2.php">Exercice 2 : bonsoir Georges !</a></li>
+        </ul>
+    </body>
+</html>
+
+<?php
+
+
